5766d9e6def5116cff85e18aad03a16e813aaf55
jay menorca <zaimenorca@gmail.com>
2025-01-12 20:47:43 -0500

    modified user/main.tf to use backend s3: added backend "s3" block; added bucket, key, region, dynamodb_table, encrypt

--- terraform-test/07-s3-backend-state/users/main.tf
+++ terraform-test/07-s3-backend-state/users/main.tf
@@ -5,22 +5,18 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+
+  backend "s3" {
+    bucket = "jayslabs-backend-state"
+    key = "dev/07-s3-backend-state/users/backend-state"
+    region = "us-east-1"
+    dynamodb_table = "jayslabs-backend-lock"
+    encrypt = true
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
-}
-
-#create an s3 bucket and enable versioning
-resource "aws_s3_bucket" "jayslabs-s3-bucket" {
-  bucket = "jayslabs-s3-bucket"
-}
-
-resource "aws_s3_bucket_versioning" "bucket_versioning" {
-  bucket = aws_s3_bucket.jayslabs-s3-bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
 }
 
 #create an IAM user
